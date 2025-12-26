c8e8801aab53f600f9e147756e927ebf62e94e7b
yyysl <352848845@qq.com>
2025-12-27 02:46:08 +0800

    Update feishu.config.js

--- feishu.config.js
+++ feishu.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  docs: 'docs', // 文档下载到 docs 目录
+  docs: 'docs',
   appId: process.env.FEISHU_APP_ID,
   appSecret: process.env.FEISHU_APP_SECRET,
   spaceId: process.env.FEISHU_SPACE_ID,
+  // 👇 这一行非常关键，告诉程序你是 Lark 用户
+  baseUrl: process.env.FEISHU_BASE_URL || 'https://open.larksuite.com', 
 }
